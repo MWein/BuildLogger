8fc76155d466b78826476b7497ccc0e47c32ae92
unknown <mweinberg21@gmail.com>
2020-10-27 00:22:43 -0500

    Hello World

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func main () {
+	fmt.Println(" --------------------------")
+	fmt.Println("|                          |")
+	fmt.Println("|     Build Log Editor     |")
+	fmt.Println("|     Weinberg Software    |")
+	fmt.Println("|                          |")
+	fmt.Println(" --------------------------")
+
+
+	// Loop until exit
+
+}
